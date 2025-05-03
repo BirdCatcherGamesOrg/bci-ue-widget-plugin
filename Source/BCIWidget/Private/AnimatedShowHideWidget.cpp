b7344448f6e0b8ce94a8c130454126eb144ab13c
BirdCatcherGames <birdcatchergames@gmail.com>
2025-05-03 13:58:14 -0700

    Add a widget with managed show/hide animation playback
    Reusable for any async animations for showing and hiding widgets.

---
+++ Source/BCIWidget/Private/AnimatedShowHideWidget.cpp
@@ -0,0 +1,70 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+
+#include "AnimatedShowHideWidget.h"
+
+void UBCIVNDKWidgetAnimatedShowHideWidget::SetVisibility(ESlateVisibility NewVisibility)
+{
+	if (NewVisibility != ESlateVisibility::Visible || NewVisibility != ESlateVisibility::Hidden)
+	{
+		GetWorld()->GetTimerManager().SetTimerForNextTick([this, NewVisibility]()
+		{
+			Super::SetVisibility(NewVisibility);
+			FBCIVNDKWidgetAnimationFinishedResponse Response;
+			Response.DesiredVisibility = NewVisibility;
+			Response.bSuccess = true;
+			OnShowHideAnimationFinished.Broadcast(Response);
+		});
+	}
+	
+	if (IsAnimationPlaying(ShowAnimation) || IsAnimationPlaying(HideAnimation))
+	{
+		GetWorld()->GetTimerManager().SetTimerForNextTick([this, NewVisibility]()
+		{
+			FBCIVNDKWidgetAnimationFinishedResponse Response;
+			Response.DesiredVisibility = NewVisibility;
+			Response.bSuccess = false;
+			OnShowHideAnimationFinished.Broadcast(Response);
+		});
+	}
+
+	DesiredVisibility = NewVisibility;
+
+	UWidgetAnimation* AnimationToPlay = nullptr;
+	if (NewVisibility == ESlateVisibility::Hidden)
+	{
+		AnimationToPlay = HideAnimation;
+
+	}
+	else if (NewVisibility == ESlateVisibility::Visible)
+	{
+		// Set visible immediately. Designers need to handle stuff like making the widget start off with 0 opacity.
+		SetVisibility(ESlateVisibility::Visible);
+		AnimationToPlay = ShowAnimation;
+	}
+
+	if (AnimationToPlay)
+	{
+		FWidgetAnimationDynamicEvent Delegate;
+		Delegate.BindDynamic(this, &UBCIVNDKWidgetAnimatedShowHideWidget::ShowHideFinished);
+		BindToAnimationFinished(AnimationToPlay, Delegate);
+		QueuePlayAnimation(AnimationToPlay);
+	}
+	
+	GetWorld()->GetTimerManager().SetTimerForNextTick([this, NewVisibility]()
+    {
+    	FBCIVNDKWidgetAnimationFinishedResponse Response;
+    	Response.DesiredVisibility = NewVisibility;
+    	Response.bSuccess = false;
+    	OnShowHideAnimationFinished.Broadcast(Response);
+    });
+}
+
+void UBCIVNDKWidgetAnimatedShowHideWidget::ShowHideFinished()
+{
+	Super::SetVisibility(DesiredVisibility);
+	FBCIVNDKWidgetAnimationFinishedResponse Response;
+	Response.DesiredVisibility = DesiredVisibility;
+	Response.bSuccess = true;
+	OnShowHideAnimationFinished.Broadcast(Response);
+}
---
+++ Source/BCIWidget/Public/AnimatedShowHideWidget.h
@@ -0,0 +1,65 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AnimatedShowHideWidget.generated.h"
+
+USTRUCT(BlueprintType)
+struct FBCIVNDKWidgetAnimationFinishedResponse
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly)
+	ESlateVisibility DesiredVisibility;
+	
+	UPROPERTY(BlueprintReadOnly)
+	bool bSuccess = false;
+
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FShowHideAnimationSignature, const FBCIVNDKWidgetAnimationFinishedResponse&, Result);
+
+/**
+ * @brief UBCIVNDKWidgetAnimatedShowHideWidget is a wrapper widget over a slot that provides show/hide animations.
+ */
+UCLASS(MinimalAPI)
+class UBCIVNDKWidgetAnimatedShowHideWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+		
+	virtual void SetVisibility(ESlateVisibility NewVisibility) override;
+
+	UPROPERTY(BlueprintAssignable)
+	FShowHideAnimationSignature OnShowHideAnimationFinished;
+	
+	/**
+	 * @brief Content is the content to display.
+	 */
+	UPROPERTY(EditDefaultsOnly, Transient, meta=(BindWidget))
+	TObjectPtr<class UNamedSlot> Content;
+
+	/**
+	 * @brief ShowAnimation is the optional animation to play when showing.
+	 */
+	UPROPERTY(EditDefaultsOnly, Transient, meta=(BindWidgetAnim))
+	TObjectPtr<class UWidgetAnimation> ShowAnimation;
+
+	/**
+	 * @brief HideAnimation is the optional animation to play when hiding.
+	 */
+	UPROPERTY(EditDefaultsOnly, Transient, meta=(BindWidgetAnim))
+	TObjectPtr<class UWidgetAnimation> HideAnimation;
+
+private:
+
+	UPROPERTY()
+	ESlateVisibility DesiredVisibility;
+	
+	UFUNCTION()
+	void ShowHideFinished();
+	
+};
